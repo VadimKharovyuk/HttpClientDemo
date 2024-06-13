98d9eaadc4db5daf62e76e64620005132c89bc15
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-06-14 01:48:14 +0300

    test

--- src/main/java/com/example/httpclient/HttpClientApplication.java
+++ src/main/java/com/example/httpclient/HttpClientApplication.java
@@ -3,11 +3,26 @@ package com.example.httpclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @SpringBootApplication
 public class HttpClientApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(HttpClientApplication.class, args);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request =HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:1010/tasks"))
+                .build();
+
+
+        var result = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(result.body());
+
     }
 
 }
